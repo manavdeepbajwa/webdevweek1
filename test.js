486e2f8a54b003df0d4160ab0145fcbf7fff2397
manavdeepbajwa <105256103+manavdeepbajwa@users.noreply.github.com>
2022-05-11 20:26:25 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-function sumX(a,b,...args){ let numSum = 0; let product = a*b; for(const arg of args){ numSum+=arg; } return[product,numSum]; }
+function sumX(a,b,...args){
+  let numSum = 0; 
+  let product = a*b;
+  for(const arg of args)
+  { 
+    numSum+=arg;
+  } 
+                          
+  return[product,numSum]; }
